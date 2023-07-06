0cec1aa3b021f6672bcfa41d059a35df3dbc9cdd
Arun-chaitanya <arunchaitanya.iitkgp@gmail.com>
2023-07-06 22:30:35 +0530

    adding sort, search, pagination and mock data

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name };
+    const testUser = (payload.userId = "64a5a34ab10638fcf226d158");
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
     throw new UnauthenticatedError("Authentication invalid");
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { register, login, updateUser } = require("../controllers/auth");
 const authenticateUser = require("../middleware/authentication");
+const testUser = require("../middleware/testUser");
 
-router.post("/register", register);
-router.post("/login", login);
-router.patch("/updateUser", authenticateUser, updateUser);
+const rateLimiter = require("express-rate-limit");
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: "Too many requests from this IP, please try again after 15 mins.",
+  },
+});
+
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
+router.patch("/updateUser", authenticateUser, testUser, updateUser);
 
 module.exports = router;
